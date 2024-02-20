8e5d78548ff96a8dc93c144e156eadc6761bfb9f
Aung Khant Myat <48421405+aungkhantmyat@users.noreply.github.com>
2024-02-20 19:28:22 +0630

    Classification.py

--- pages/1_Classification.py
+++ pages/1_Classification.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import joblib
 import pyidaungsu as pds
 from sklearn.feature_extraction.text import TfidfVectorizer
 
